5dab436c5d18f9f68339ab8afa55eb15827cbb13
Benna96 <vissersofia@gmail.com>
2024-12-17 21:23:56 +0200

    Implement day 1 part 2

--- AdventOfCode.Tests/Day01SolverTests.cs
+++ AdventOfCode.Tests/Day01SolverTests.cs
@@ -18,4 +18,21 @@ public class Day01SolverTests
         string solution = Day01Solver.SolvePart1(input);
         solution.Should().Be(correctSolution);
     }
+
+    [Fact]
+    public void Solves_part_2_correctly()
+    {
+        const string input = """
+                             3   4
+                             4   3
+                             2   5
+                             1   3
+                             3   9
+                             3   3
+                             """;
+        const string correctSolution = "31";
+
+        string solution = Day01Solver.SolvePart2(input);
+        solution.Should().Be(correctSolution);
+    }
 }
--- AdventOfCode/Day01Solver.cs
+++ AdventOfCode/Day01Solver.cs
@@ -29,4 +29,31 @@ public class Day01Solver
 
         return distanceSum.ToString();
     }
+
+    public static string SolvePart2(string input)
+    {
+        List<int> leftIds = [];
+        List<int> rightIds = [];
+
+        using (var reader = new StringReader(input))
+        {
+            while (reader.ReadLine() is { } line)
+            {
+                var split = line.Split("   ");
+                leftIds.Add(int.Parse(split[0]));
+                rightIds.Add(int.Parse(split[1]));
+            }
+        }
+
+        Dictionary<int, int> rightIdCounts = [];
+        foreach (var id in rightIds)
+        {
+            var idCount = rightIdCounts.GetValueOrDefault(id, 0);
+            ++idCount;
+            rightIdCounts[id] = idCount;
+        }
+
+        var similarityScore = leftIds.Sum(id => id * rightIdCounts.GetValueOrDefault(id, 0));
+        return similarityScore.ToString();
+    }
 }
