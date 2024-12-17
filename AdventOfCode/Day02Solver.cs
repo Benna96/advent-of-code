6d137246e884dbc4db49594cce8d188046d8cd89
Benna96 <vissersofia@gmail.com>
2024-12-17 23:51:35 +0200

    Implement day 2 part 1

---
+++ AdventOfCode/Day02Solver.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode;
+
+public class Day02Solver
+{
+    public static string SolvePart1(string input)
+    {
+        var levelReports = ReadLevelReportsFrom(input);
+        var safeReports = levelReports.Where(IsSafe);
+
+        return safeReports.Count().ToString();
+
+        static IList<IList<int>> ReadLevelReportsFrom(string inputString)
+        {
+            IList<IList<int>> readLevelReports = [];
+
+            using (var reader = new StringReader(inputString))
+            {
+                while (reader.ReadLine() is { } line)
+                {
+                    var split = line.Split(' ');
+                    var levels = split.Select(int.Parse);
+                    readLevelReports.Add(levels.ToList());
+                }
+            }
+
+            return readLevelReports;
+        }
+
+        static bool IsSafe(IList<int> levels)
+        {
+            var previousLevels = levels.SkipLast(1);
+            var nextLevels = levels.Skip(1);
+
+            var increases = nextLevels.Zip(previousLevels, (next, prev) => next - prev).ToArray();
+
+            if (increases.Any(x => Math.Abs(x) is < 1 or > 3))
+                return false;
+
+            if (increases.Select(Math.Sign).Distinct().Skip(1).Any())
+                return false;
+
+            return true;
+        }
+    }
+}
