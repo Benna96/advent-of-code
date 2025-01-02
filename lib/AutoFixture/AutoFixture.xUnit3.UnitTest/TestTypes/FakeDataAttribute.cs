38e1fca1d176fa2450dd04f13acff9ec05478ef1
Benna96 <vissersofia@gmail.com>
2025-01-02 20:44:53 +0200

    Copy & fix files from v2 to v3
    Use AutoFixture.xUnit2 files as base, & modify so the project compiles.
    - Namespaces: AutoFixture.XUnit2 -> AutoFixture.XUnit3
    - DataAttribute implementation
      - GetData: Wrap old GetData
      - SupportsDiscoveryEnumeration: Transfer from DataDiscovererAttribute
    - Remove NoPreDiscoveryDataDiscovererAttribute, removed from xUnit.v3
    - InlineAutoDataAttributeTests.InjectsInlineValues: Cast the null case
      as object, otherwise it passes in null params which gets caught.

---
+++ lib/AutoFixture/AutoFixture.xUnit3.UnitTest/TestTypes/FakeDataAttribute.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Sdk;
+using Xunit.v3;
+
+namespace AutoFixture.Xunit3.UnitTest.TestTypes
+{
+    public class FakeDataAttribute : DataAttribute
+    {
+        private readonly MethodInfo expectedMethod;
+        private readonly IEnumerable<object[]> output;
+
+        public FakeDataAttribute(MethodInfo expectedMethod, IEnumerable<object[]> output)
+        {
+            this.expectedMethod = expectedMethod;
+            this.output = output;
+        }
+
+        public /*override*/ IEnumerable<object[]> GetData(MethodInfo methodUnderTest)
+        {
+            Assert.Equal(this.expectedMethod, methodUnderTest);
+
+            return this.output;
+        }
+
+        public override ValueTask<IReadOnlyCollection<ITheoryDataRow>> GetData(MethodInfo testMethod, DisposalTracker disposalTracker)
+        {
+            return new ValueTask<IReadOnlyCollection<ITheoryDataRow>>(this.GetData(testMethod)
+                .Select(data => new TheoryDataRow(data))
+                .ToArray());
+        }
+
+        public override bool SupportsDiscoveryEnumeration()
+        {
+            return false;
+        }
+    }
+}
---
+++ lib/AutoFixture/TestTypeFoundation/TypeWithOverloadedMembers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection;
+
+namespace TestTypeFoundation
+{
+    public class TypeWithOverloadedMembers
+    {
+        public object SomeProperty { get; set; }
+
+        public void DoSomething()
+        {
+        }
+
+        public void DoSomething(object obj)
+        {
+        }
+
+        public void DoSomething(object x, object y)
+        {
+        }
+
+        public void DoSomething(object x, object y, object z)
+        {
+        }
+
+        public static MethodInfo GetDoSomethingMethod(params Type[] parameterTypes) =>
+            typeof(TypeWithOverloadedMembers)
+                .GetMethod(nameof(DoSomething), parameterTypes);
+    }
+}
